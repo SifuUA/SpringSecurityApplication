945e3164d28b8a43e059da815d9c990aaf3924a4
SifuUA <alekseykres@gmail.com>
2017-06-15 21:22:49 +0300

    add Controller

--- src/java/net/okres/springsecurityapplication/service/SecurityServiceImpl.java
+++ src/java/net/okres/springsecurityapplication/service/SecurityServiceImpl.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -25,11 +27,24 @@ public class SecurityServiceImpl implements SecurityService{
     @Override
     public String findLoggedInUsername() {
         Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();//poluchaem detali nashego usera
+        if (userDetails instanceof UserDetails){
+            return ((UserDetails)userDetails).getUsername();//poluchenie imeni zaloginenogo usera
+        }
         return null;
     }
 
     @Override
-    public void autoLogin(String name, String password) {
+    public void autoLogin(String username, String password) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
+        // peredaem prava(name, password, razreshenie)
+        authenticationManager.authenticate(authenticationToken);//avtorizaciya
 
+        if (authenticationToken.isAuthenticated()){
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            logger.debug(String.format("Successfully %s auto logged in.", username));
+        }
     }
 }
